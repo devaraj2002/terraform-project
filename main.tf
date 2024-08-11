e705795609b491d4fcd3adf1cdd788ab6a08d99e
devaraj <155232518+devaraj2002@users.noreply.github.com>
2024-08-11 16:36:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,15 +35,7 @@ resource "aws_security_group" "sg" {
 }
 
  
-resource "aws_subnet" "private"{
-    vpc_id=aws_vpc.one.id
-    cidr_block="10.0.0.0/16"
-    availability_zone="us-east-1a"
-    tags={
-        Name="private-subnet"
-    }
 
-}
 
 resource "aws_subnet" "public"{
     vpc_id=aws_vpc.one.id
